2655f5882bf12a3e6470f828f177332dac1c099d
Evan <evan@datarank.com>
2015-06-15 13:36:51 -0400

    Refactored base case

--- homework2.cpp
+++ homework2.cpp
@@ -3,81 +3,85 @@
 #include "matrix.h"
 using namespace std;
 
-int recurseMatrix(Matrix &m, int x, int y, int xSize, int ySize);
-
-void doit(Matrix &m)
-{
-	int shortestPath;
-	string stringPath = "";
-	// print the whole matrix
-	m.print();
-
-	// get the number of rows/cols in the matrix
-	cout << "rows: " << m.getRows() << ", cols: " << m.getCols() << endl;
-
-
-	shortestPath = recurseMatrix(m, 1, 1, m.getRows(), m.getCols());
-
-	// Answer is: 
-	// false
-	// false
-	// true
-	// true
-	// true
-	// false
-	// true
-	// false
-
-	cout << "Ending: " << shortestPath << "\n";
+bool testInputs (int argc, char **argv, Matrix &m) {
+    cout << "Testing\n";
+    if(argc < 2)
+    {
+        cout << "Please enter a filename when you call the program!\n";
+        return false;
+    }
+    
+    if(!m.loadFile(argv[1]))
+    {
+        cout << "Invalid file!\n";
+        return false;
+    }
+    return true;
 }
 
 int recurseMatrix(Matrix &m, int x, int y, int xSize, int ySize) {
-	int wentDown = 1000;
-	int wentRight = 1000;
+    int wentDown = 1000;
+    int wentRight = 1000;
 
-	if ((x == xSize) && (y == ySize)) {
-		return m.get(x - 1, y - 1);
-	}
-	else {
+    // base case if in the bottom-right index of the array
+    if ((x == xSize) && (y == ySize)) {
+        return m.get(x - 1, y - 1);
+    }
+    else {
 
-		if (x < xSize) {
-			wentDown = recurseMatrix(m, x + 1, y, xSize, ySize);
-		}
+        // else recursively call recurseMatrix to go both down and right
+        if (x < xSize) {
+            wentDown = recurseMatrix(m, x + 1, y, xSize, ySize);
+        }
 
-		if (y < ySize) {
-			wentRight = recurseMatrix(m, x, y + 1, xSize, ySize);
-		}
+        if (y < ySize) {
+            wentRight = recurseMatrix(m, x, y + 1, xSize, ySize);
+        }
 
-		cout << "At Coordinates x: " << x  << ", y: " << y << "\n";
-		if (wentDown < wentRight) {
-			wentDown = wentDown + m.get(x - 1, y - 1);
-			return wentDown;
-		}
-		else {
-			wentRight = wentRight + m.get(x - 1, y - 1);
-			return wentRight;
-		}
-	}
+        // return the smaller of the two paths
+        // cout << "At Coordinates x: " << x  << ", y: " << y << "\n";
+        if (wentDown < wentRight) {
+            wentDown = wentDown + m.get(x - 1, y - 1);
+            return wentDown;
+        }
+        else {
+            wentRight = wentRight + m.get(x - 1, y - 1);
+            return wentRight;
+        }
+    }
 }
 
 
 int main(int argc, char **argv)
 {
-	Matrix m;
-	cout << "Testing\n";
-	if(argc < 2)
-	{
-		cout << "Please enter a filename when you call the program!\n";
-		return 1;
-	}
-	
-	if(!m.loadFile(argv[1]))
-	{
-		cout << "Invalid file!\n";
-		return 1;
-	}
-	
-	doit(m);
-	
-	return 0;
+    Matrix m;
+    int shortestPath;
+    string stringPath = "";
+    bool legalInputs = false;
+
+    legalInputs = testInputs(argc, argv, m);
+    
+    if (legalInputs) {
+        // print the whole matrix
+        m.print();
+
+        // get the number of rows/cols in the matrix
+        cout << "rows: " << m.getRows() << ", cols: " << m.getCols() << endl;
+
+        // call recursive 
+        shortestPath = recurseMatrix(m, 1, 1, m.getRows(), m.getCols());
+
+        // Correct path should be: 
+        // right
+        // right
+        // down
+        // down
+        // down
+        // right
+        // down
+        // right
+
+        cout << "Shortest Path: " << shortestPath << "\n";
+    }
+    return 0;
 }
--- matrix.h
+++ matrix.h
@@ -7,28 +7,28 @@ using namespace std;
 
 class Matrix
 {
-	public:
-		Matrix(int pRows = 10, int pCols = 10);
-		Matrix(string file);
-		~Matrix();
+    public:
+        Matrix(int pRows = 10, int pCols = 10);
+        Matrix(string file);
+        ~Matrix();
 
-		void resize(int pRows, int pCols);
-		void print(ostream &sout = cout) const;
+        void resize(int pRows, int pCols);
+        void print(ostream &sout = cout) const;
 
-		bool loadFile(string file);
-		bool saveFile(string file) const;
+        bool loadFile(string file);
+        bool saveFile(string file) const;
 
-		int getRows() const;
-		int getCols() const;
+        int getRows() const;
+        int getCols() const;
 
-		void set(int row, int col, int value);
-		int get(int row, int col) const;
+        void set(int row, int col, int value);
+        int get(int row, int col) const;
 
-		const int *operator[](int i) const;
-		int *operator[](int i);
-	private:
-		int **array;
-		int rows, cols;
+        const int *operator[](int i) const;
+        int *operator[](int i);
+    private:
+        int **array;
+        int rows, cols;
 };
 
 #endif
